4e8080762896dc2b1d869a0b043c42bd09e065f2
PM369 <pallabkumarmallick189@gmail.com>
2021-08-03 18:38:23 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3723f104dff3907f4572bc69be215225'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd003e424-be1a-4cd9-a6ef-2498bddcbfe2'
+};
